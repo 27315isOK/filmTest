1a8fe8eb0e9eafee8f89b442f626169a88199e9c
27315isOK <1808454349@qq.com>
2022-02-08 15:14:37 +0800

    更改测试改了mine.vue

--- src/views/home/mine.vue
+++ src/views/home/mine.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>
+    <div class="bg">
         <h2>mine</h2>
+        <div class="bg1">
+            <input>
+            <div>{{info}}</div>
+        </div>
     </div>
 </template>
 
 <script>
+
 export default {
-    
+    data(){
+        return{
+            info:'rember'
+        };
+    },
+    methods:{
+
+    },
+    components:{
+
+    },
+    mounted(){
+
+    }
 }
 </script>
 <style scoped>
-
+    .bg{
+        position: relative;
+    }
+    .bg1{
+        position: absolute;
+    }
+    .bg h2{
+        color: #f60;
+    }
 </style>
